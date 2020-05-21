35bdab0c5411a1e9cc60109bbabdaa6eddd1e088
gkarlos <gkarloscodes@gmail.com>
2020-05-21 22:44:25 +0200

    notifications button enable disable on click

--- src/renderer/ui/toolbars/util/NotificationButton.js
+++ src/renderer/ui/toolbars/util/NotificationButton.js
@@ -13,19 +13,30 @@ class NotificationButton extends Component {
     this.node = null;
     this.enabled = true;
     this.tooltip = NotificationButton.enabledTooltip
+    this.onClickCallbacks = []
   }
 
   enable() {
     this.enabled = true
+    this.tooltip = NotificationButton.enabledTooltip
+
     if( this.rendered) {
-      
+      this.node.empty()
+      this.node.attr('data-original-title', this.tooltip)
+               .tooltip('show')
+      $(`<i class="fas fa-bell fa-fw center-vertically"></i>`).appendTo(this.node)
     }
   }
 
   disable() {
     this.enabled = false 
-    if ( this.rendered) {
+    this.tooltip = NotificationButton.disabledTooltip
 
+    if ( this.rendered) {
+      this.node.empty()
+      this.node.attr('data-original-title', this.tooltip)
+               .tooltip('show')
+      $(`<i class="far fa-bell-slash fa-fw center-vertically"></i>`).appendTo(this.node)
     }
   }
 
@@ -33,22 +44,41 @@ class NotificationButton extends Component {
     if ( !this.rendered) {
       this.node = $(`
         <div id='${this.id}' data-toggle='tooltip' title='${this.tooltip}'>
-          <i class="far fa-bell center-vertically"></i>
+          ${this.enabled
+            ? '<i class="fas fa-bell fa-fw center-vertically"></i>'
+            : '<i class="fas fa-bell fa-fw center-vertically"></i>'
+          }
         </div>
      `).css('font-size', '20px')
-      //  .css("margin-right", "10px")
        .css("opacity", "0.6")
        .css('font-size', '18px')
+       .css('cursor', 'pointer')
+
+      this.node.tooltip()
+
+      this.onClick(() => {
+        if ( this.enabled)
+          this.disable()
+        else
+          this.enable()
+      })
+
+
+      this.onClickCallbacks.forEach(callback => this.node.on('click', callback))
     }
 
-    if ( this.enabled) {
+    if ( this.enabled)
       this.node.appendTo(this.container)
-    }
-
-    
 
     this.rendered = true;
     return this;
+  }
+
+  onClick(callback) {
+    if ( this.rendered)
+      this.node.on('click', callback)
+    else
+      this.onClickCallbacks.push(callback)
   }
 }
 
