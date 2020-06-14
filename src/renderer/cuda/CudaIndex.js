e55215ed2880559ebc53d558e1a05ffd6416680e
gkarlos <gkarloscodes@gmail.com>
2020-06-14 03:30:49 +0200

    [cuda] CudaIndex linearize/delinearize now accept a CudaDim. Also update tests to reflect to the change

---
+++ src/renderer/cuda/CudaIndex.js
@@ -0,0 +1,242 @@
+/** @ignore @typedef {import("@renderer/cuda/CudaDim")} CudaDim */
+
+
+/**
+ * Represents a 2D index value. Can be used index a thread or
+ * index into an array. **Row-major** order is assumed
+ * 
+ * @memberof module:cuda
+ */
+class CudaIndex {
+  /** @type {Number} */
+  #col
+  /** @type {Number} */
+  #row
+
+  /**
+   * Transform 2D index to 1D index
+   * @param {CudaIndex} index A 2D index
+   * @param {CudaDim} dim A cuda dim description
+   * @throws {Error} If args are invalid. **Only 2D CudaDims are supported**
+   * @returns {Number} 
+   */
+  static linearize(index, dim) {
+    if ( index === undefined) throw new Error('Missing required argument `index`')
+    if ( dim === undefined)  throw new Error('Missing required argument `dim`')
+    if ( dim.is3D()) throw new Error('Invalid dim. 3 dimensions are not supported')
+    if ( index.col >= dim.x || index.row >= dim.y)
+      throw new Error(`Invalid index '${index.toString()}' for dimensions [${rows},${cols}]`)
+
+    if ( dim.y == 1) //1D
+      return index.col
+
+    return (index.row * dim.x) + index.col
+  }
+
+  /**
+   * Transform a 1-dimensional index to a 2-dimensional index
+   * @param {Number|CudaIndex} index A 1-Dimensional index. If a CudaIndex is passed and the y-dimension > 1 then it is returned as is.
+   * @param {CudaDim} dim A cuda dim description
+   * @throws {Error} If args are invalid. **Only 2D CudaDims are supported**
+   * @returns {CudaIndex} 
+   */
+  static delinearize(index, dim) {
+    if ( index === undefined) throw new Error('Missing required argument `index`')
+    if ( dim === undefined)  throw new Error('Missing required argument `dim`')
+    if ( dim.is3D()) throw new Error('Invalid dim. 3 dimensions are not supported')
+
+    let row, col
+    if ( Number.isInteger(index)) {
+      if ( dim.y === 1)
+        return new CudaIndex(index)
+      row = Math.floor(index / dim.x)
+      col = index % dim.x
+    } else {
+      if ( !(index instanceof CudaIndex)) throw new Error('Argument `index` must be an Integer or a CudaIndex')
+      // if ( index.col >= dim.x || index.row >= dim.y) throw new Error(`Argument missmatch. CudaIndex ${index.toString()} not valid for dims ${dim.toString()}`)
+      if ( index.row > 0) //2D index already
+        return index
+      row = Math.floor(index.col / dim.x)
+      col = index.col % dim.x
+    }
+
+    if ( row >= dim.y || col >= dim.x)
+      throw new Error(`Argument missmatch. CudaIndex ${index.toString()} not valid for dims ${dim.toString()}`)
+
+    return new CudaIndex(row, col)
+  }
+
+  /** @protected */
+  _argCheck(row,col) {
+    if ( row && (!Number.isInteger(row) || row < 0))
+      throw new Error('Invalid argument `row`: must be integer > 0')
+    if ( col && (!Number.isInteger(col) || col < 0))
+      throw new Error('Invalid argument `col`: must be integer > 0')
+  }
+
+  /**
+   * Creates a new `CudaIndex` object
+   *
+   * @param {Number} [row] Value for the row, (y) dimension.
+   * @param {Number} col Value for the col (x) dimension.
+   * @description
+   *  - When two arguments are passed the first one defines the row and the second one the column.
+   *  - When one argument is passed it defines the colum.
+   * This is to be consistent with common, C-like, row-major indexing notation
+   * @example
+   *  let index1 = new Index(1,2) // 3rd element of the 2nd row
+   * @example
+   *  let index2 = new Index(9,1) // 2nd element of the 10th row
+   * @example
+   *  let index3 = new Index(9,0) // 1st element of the 10th row
+   * @example
+   *  let index4 = new Index(9)   // 10th element of the 1st row
+   * @example
+   *  let index5 = new Index(0,9) // Equivalent to index4
+   */
+  constructor(row, col) {
+    // if one argument passed treat it as col index
+    if ( col === undefined) {
+      col = row
+      row = 0
+    }
+    this._argCheck(row, col)
+    this.#col = col
+    this.#row = row
+  }
+  
+  /** 
+   * Retrieve the x-coordinate
+   * @returns {Number}
+   */
+  get col() { return this.#col }
+
+  /**
+   * Retrieve the y-coordinate
+   * @returns {Number}
+   */
+  get row() { return this.#row }
+
+  /**
+   * Retrieve the row value (y-coordinate)
+   * Alias for {@link CudaIndex#row}
+   * @returns {Number}
+   */
+  get y() { return this.row }
+
+  /**
+   * Retrieve the col value (x-coordinate)
+   * Alias for {@link CudaIndex#col}
+   * @returns {Number}
+   */
+  get x() { return this.col }
+
+  /**
+   * Increase the index
+   *
+   * @param {Number} [row]
+   * @param {Number} col
+   * @returns {CudaIndex} this
+   * @throws {Error} When operation results in invalid index. 
+   *                 e.g a negative value for some dimension
+   * @description
+   *  - When two arguments are passed the first one defines the row increase and the second one the column increase
+   *  - When one argument is passed it defines the column increase.
+   * This is to be consistent with common, C-like, row-major indexing notation
+   * @example
+   * let index = new Index(1,1) // 2nd element of 2nd row
+   * index.inc(1,1)             // 3nd element of 3rd row
+   * index.inc(1)               // 4th element of 3rd row. equivalent to index.inc(0,1)
+   * index.inc(-10)             // Error
+   */
+  inc(row, col) {
+
+    if ( row === undefined)
+      return this;
+
+    if ( col === undefined) {
+      col = row
+      row = 0
+    }
+
+    this.#row += row
+    if ( this.#row < 0)
+      throw new Error("Invalid argument `row`. Results in negative value")
+    
+    this.#col += col
+    if ( this.#col < 0)
+      throw new Error("Invalid argument `col`. Results in negative value")
+
+    return this
+  }
+
+  /**
+   * Decrease the index
+   *
+   * @param {Number} [row]
+   * @param {Number} col
+   * @returns {CudaIndex} this
+   * @throws {Error} When operation results in invalid index. 
+   *                 e.g a negative value for some dimension
+   * @description
+   *  - When two arguments are passed the first one defines the row increase and the second one the column increase
+   *  - When one argument is passed it defines the column increase.
+   * This is to be consistent with common, C-like, row-major indexing notation
+   * @example
+   * let index = new Index(1,1) // 2nd element of 2nd row
+   * index.dec(1,1)             // 1st element of 1st row
+   * index.dec(0,0)             // No change
+   * index.dec(10)              // Error
+   * index.dec(-9)              // 10th element of first row. Equivalent to dec(0,-9)
+   * index.dec(2,2)             // Error
+   * index.dec(0,9)             // 1st element of 1st row. Equivalent to dec(9) 
+   */
+  dec(row, col) {
+
+    if ( row === undefined)
+      return this;
+
+    if ( col === undefined) {
+      col = row
+      row = 0
+    }
+
+    this.#row -= row
+    if ( this.#row < 0)
+      throw new Error("Invalid argument `row`. Results in negative value")
+    
+    this.#col -= col
+    if ( this.#col < 0)
+      throw new Error("Invalid argument `col`. Results in negative value")
+
+    return this
+  }
+  /**
+   * Compare equality with another index
+   * @param {Index} other 
+   * @return {Boolean}
+   */
+  equals(other) {
+    if ( !other || !(other instanceof CudaIndex))
+      return false
+    return this.x == other.x && this.y == other.y
+  }
+
+  /**
+   * Creates and return a copy of the `Point` object
+   * @returns {Index}
+   */
+  clone() {
+    let copy = new Index(this.#row, this.#col)
+    return copy
+  }
+
+  /**
+   * Get a String representation of the index
+   * @return {String}
+   */
+  toString() { return `[${this.y},${this.x}]` }
+
+}
+
+module.exports = CudaIndex
---
+++ test/unit/cuda/CudaIndex.test.js
@@ -0,0 +1,179 @@
+require('module-alias/register')
+const Index = require('@renderer/cuda').Index
+const Dim = require('@renderer/cuda').Dim
+
+const expect = require('chai').expect
+
+describe('renderer/cuda/CudaIndex', () => {
+  describe('(static) delinearize', () => {
+    it('should throw with missing args', () => {
+      expect(() => Index.delinearize()).to.throw
+      expect(() => Index.delinearize(null)).to.throw
+      expect(() => Index.delinearize(undefined)).to.throw
+      expect(() => Index.delinearize(null, null)).to.throw
+      expect(() => Index.delinearize(undefined, undefined)).to.throw
+    })
+
+    it('should throw with invalid args', () => {
+      expect(() => Index.delinearize(new Index(10), -2)).to.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(-2))).to.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(20, -2))).to.throw
+    })
+
+    it('should throw on 3D Dim', () => {
+      expect(() => Index.delinearize(new Index(10), new Dim(2,1,3))).to.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(20,20,20))).to.throw
+    })
+
+    it('should not throw on 1D,2D Dim', () => {
+      expect(() => Index.delinearize(new Index(10), new Dim(2,1))).to.not.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(2,1,1))).to.not.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(20,20,20))).to.throw
+      expect(() => Index.delinearize(new Index(10), new Dim(2,1,20))).to.throw
+    })
+
+    it('should throw on missmatch', () => {
+      let index = new Index(100);
+      expect(() => Index.delinearize(index, new Dim(100))).to.throw
+      expect(() => Index.delinearize(100, new Dim(100))).to.throw
+      expect(() => Index.delinearize(index, new Dim(5, 20))).to.throw
+    })
+
+    it('should not throw on valid args', () => {
+      expect(() => Index.delinearize(new Index(1), new Dim(2))).to.not.throw
+      expect(() => Index.delinearize(1, new Dim(2))).to.not.throw
+      expect(() => Index.delinearize(new Index(1), new Dim(2, 2))).to.not.throw
+      expect(() => Index.delinearize(1, new Dim(2, 2))).to.not.throw
+      expect(() => Index.linearize(new Index(0), new Dim(1))).to.not.throw
+      expect(() => Index.linearize(0, new Dim(1))).to.not.throw
+    })
+
+    it('should compute the right values (1)', () => {
+      expect(Index.delinearize(new Index(1), new Dim(200, 200)).equals(new Index(1))).to.be.true
+      expect(Index.delinearize(1, new Dim(200, 200)).equals(new Index(1))).to.be.true
+      expect(Index.delinearize(new Index(1), new Dim(200, 200)).equals(new Index(0,1))).to.be.true
+      expect(Index.delinearize(1, new Dim(200, 200)).equals(new Index(0,1))).to.be.true
+    })
+
+    it('should compute the right values (2)', () => {
+      let index = new Index(0)
+      expect(Index.delinearize(new Index(0), new Dim(1)).equals(new Index(0))).to.be.true
+      expect(Index.delinearize(new Index(0), new Dim(10)).equals(new Index(0,0))).to.be.true
+      expect(Index.delinearize(new Index(0,0), new Dim(100)).equals(new Index(0))).to.be.true
+      expect(Index.delinearize(new Index(0,0), new Dim(1000)).equals(new Index(0,0))).to.be.true
+      expect(Index.delinearize(0, new Dim(10000)).equals(new Index(0,0))).to.be.true
+
+      expect(Index.delinearize(new Index(0), new Dim(1,1)).equals(new Index(0))).to.be.true
+      expect(Index.delinearize(new Index(0), new Dim(10,10)).equals(new Index(0,0))).to.be.true
+      expect(Index.delinearize(new Index(0,0), new Dim(100,100)).equals(new Index(0))).to.be.true
+      expect(Index.delinearize(new Index(0,0), new Dim(1000,1000)).equals(new Index(0,0))).to.be.true
+      expect(Index.delinearize(0, new Dim(10000, 200)).equals(new Index(0,0))).to.be.true
+    })
+
+    it('should compute the right values (3)', () => {
+      expect(Index.delinearize(new Index(999,0), new Dim(1,1000)).equals(new Index(999))).to.be.false
+      expect(Index.delinearize(new Index(999,0), new Dim(1,1000)).equals(new Index(999,0))).to.be.true
+
+      expect(Index.delinearize(new Index(999), new Dim(1000,1)).equals(new Index(999))).to.be.true
+      expect(Index.delinearize(new Index(999), new Dim(1000,1)).equals(new Index(0,999))).to.be.true
+      expect(Index.delinearize(999, new Dim(1000,1)).equals(new Index(999))).to.be.true
+    })
+
+    it('should compute the right values (4)', () => {
+      expect(Index.delinearize(new Index(1002), new Dim(500,5)).equals(new Index(2,2))).to.be.true
+      expect(Index.delinearize(1002, new Dim(500,5)).equals(new Index(2,2))).to.be.true
+      expect(Index.delinearize(1002, new Dim(500,5)).equals(new Index(2,2,1))).to.be.true
+    })
+
+    it('should compute the right values (5)', () => {
+      expect(Index.delinearize(new Index(2010), new Dim(500,5)).equals(new Index(4,10))).to.be.true
+      expect(Index.delinearize(new Index(2010), new Dim(5,500)).equals(new Index(402))).to.be.false
+      expect(Index.delinearize(new Index(2010), new Dim(5,500)).equals(new Index(402,0))).to.be.true
+      expect(Index.delinearize(new Index(2010), new Dim(5,500)).equals(new Index(402,0,1))).to.be.true
+      expect(Index.delinearize(2010, new Dim(5,500)).equals(new Index(402))).to.be.false
+      expect(Index.delinearize(2010, new Dim(5,500)).equals(new Index(402,0))).to.be.true
+      expect(Index.delinearize(2010, new Dim(5,500)).equals(new Index(402,0,1))).to.be.true
+    })
+  })
+
+  describe('(static) linearize', () => {
+    it('should throw with missing args', () => {
+      expect(() => Index.linearize()).to.throw
+      expect(() => Index.linearize(null)).to.throw
+      expect(() => Index.linearize(undefined)).to.throw
+      expect(() => Index.linearize(null, null)).to.throw
+      expect(() => Index.linearize(undefined, undefined)).to.throw
+    })
+
+    it('should throw with invalid args', () => {
+      expect(() => Index.linearize(new Index(10), -2)).to.throw
+      expect(() => Index.linearize(new Index(10), new Dim(-2))).to.throw
+      expect(() => Index.linearize(new Index(10), new Dim(20, -2))).to.throw
+      expect(() => Index.linearize(new Index(10), new Dim(0,0,0))).to.throw
+    })
+
+    it('should throw on 3D Dim', () => {
+      expect(() => Index.linearize(new Index(10), new Dim(2,1,3))).to.throw
+      expect(() => Index.linearize(new Index(10), new Dim(20,20,20))).to.throw
+    })
+
+    it('should not throw on 1D,2D Dim', () => {
+      expect(() => Index.linearize(new Index(10), new Dim(2,1))).to.not.throw
+      expect(() => Index.linearize(new Index(10), new Dim(2,1,1))).to.not.throw
+      expect(() => Index.linearize(new Index(10), new Dim(20,20,20))).to.throw
+      expect(() => Index.linearize(new Index(10), new Dim(2,1,20))).to.throw
+    })
+
+    it('should throw on missmatch', () => {
+      let index = new Index(1,10,10);
+      expect(() => Index.linearize(index, new Dim(200))).to.throw
+      expect(() => Index.linearize(index, new Dim(200, 1, 5))).to.throw
+      expect(() => Index.linearize(index, new Dim(200, 5, 2))).to.throw
+      expect(() => Index.linearize(index, new Dim(1, 11, 11))).to.throw
+      expect(() => Index.linearize(index, new Dim(1))).to.throw
+    })
+
+    it('should not throw on valid args', () => {
+      let index = new Index(1)
+      expect(() => Index.linearize(index, new Dim(2))).to.not.throw
+      expect(() => Index.linearize(index, new Dim(2, 2, 2))).to.not.throw
+
+      index = new Index(0)
+      expect(() => Index.linearize(index, new Dim(1))).to.not.throw
+    })
+
+    it('should compute the right values (1)', () => {
+      let index = new Index(1)
+      expect(Index.linearize(index, new Dim(200, 200))).to.equal(1)
+      expect(Index.linearize(index, new Dim(2,2))).to.equal(1)
+      expect(Index.linearize(index, new Dim(2))).to.equal(1)
+      expect(Index.linearize(index, new Dim(2,1))).to.equal(1)
+      expect(Index.linearize(index, new Dim(2,1,1))).to.equal(1)
+    })
+
+    it('should compute the right values (2)', () => {
+      let index = new Index(2,5)
+      expect(Index.linearize(index, new Dim(10, 5))).to.equal(25)
+    })
+
+    it('should compute the right values (3)', () => {
+      let index = new Index(0)
+      expect(Index.linearize(index, new Dim(1))).to.equal(0)
+    })
+
+    it('should compute the right values (4)', () => {
+      let index = new Index(10,2)
+      expect(Index.linearize(index, new Dim(100, 100))).to.equal(1002)
+    })
+
+    it('should compute the right values (5)', () => {
+      let index = new Index(2000)
+      expect(Index.linearize(index, new Dim(10000))).to.equal(2000)
+    })
+
+    it('should compute the right values (6)', () => {
+      let index = new Index(20, 5)
+      expect(Index.linearize(index, new Dim(100, 10000))).to.equal(2005)
+    })
+  })
+})
