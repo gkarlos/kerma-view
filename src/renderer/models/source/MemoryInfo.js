9a6f3978ee4a73c0eae3121f8de7115bcced624e
gkarlos <gkarloscodes@gmail.com>
2020-07-14 15:28:31 +0200

    [source] Add MemoryInfo class

---
+++ src/renderer/models/source/MemoryInfo.js
@@ -0,0 +1,75 @@
+const SourceInfo = require('./SourceInfo')
+const SourceRange = require('./SourceRange')
+const _ = require('@renderer/app')._
+
+/** @ignore @type import("@renderer/models/source/SourceRange") SourceRange*/
+
+/**
+ * @memberof module:source
+ */
+class MemoryInfo extends SourceInfo {
+
+  /** @type {String}   */ #type
+  /** @type {String}   */ #name
+  /** @type {String[]} */ #qualifiers
+  
+  /**
+   * Create a new MemoryInfo object
+   * @param {Object}      opts
+   * @param {String}      opts.type
+   * @param {String}      opts.name
+   * @param {String[]}    opts.qualifiers
+   * @param {String}      opts.filename
+   * @param {SourceRange} opts.range
+   */
+  constructor(opts={}) {
+    super(opts)
+    this.#type = opts.type
+    this.#name = opts.name
+    this.#qualifiers = opts.qualifiers || []
+  }
+
+  /** @type {String} */
+  get type() { return this.#type }
+
+  /** @type {String} */
+  get name() { return this.#name }
+
+  /** @type {String} */
+  get qualifiers() { return this.#qualifiers }
+
+  /** @returns {Boolean} */
+  hasQualifier(qualifier) {
+    return this.#qualifiers.find(qualifier)
+  }
+
+  /** @returns {void} */
+  addQualifier(qualifier) {
+    this.#qualifiers.push(qualifier)
+  }
+
+  /** @returns {Boolean} */
+  removeQualifier(qualifier) {
+    let res = false
+    for ( let i = 0; i < this.#qualifiers.length; ++i)
+      if ( this.#qualifiers[i] === qualifier) {
+        this.#qualifiers.splice(i, 1)
+        res = true
+      }
+    return res
+  }
+
+  /**
+   * 
+   * @param {MemoryInfo} other 
+   */
+  equals(other) {
+    return (other instanceof MemoryInfo) 
+      && super.equals(other)
+      && this.#type === other.type
+      && this.#name === other.name
+      && _.isEqual(this.#qualifiers, other.qualifiers)
+  }
+}
+
+module.exports = MemoryInfo
