8771a073c591508043a1e6fd0b2e1447e81c8d5e
George Karlos <gkarloscodes@gmail.com>
2020-07-18 21:07:22 +0200

    [memory] update AddrSpace

--- src/renderer/models/memory/AddressSpace.js
+++ src/renderer/models/memory/AddressSpace.js
@@ -1,5 +1,31 @@
-module.exports = {
-  LOCAL : 0,
-  SHARED : 1,
-  GLOBAL : 2
+/**
+ * @memberof module:memory
+ */
+class AddrSpace {
+
+  /** @type {String} */ #name
+  /** @type {Number} */ #value
+
+  constructor(name, value) {
+    this.#name = name
+    this.#value = value
+  }
+
+  /** @type {String} */
+  get name() { return this.#name }
+
+  /** @type {Number} */
+  get value() { return this.#value }
+
+  /**
+   * @param {AddrSpace} other
+   * @returns {Boolean}
+   */
+  equals(other) {
+    return ( other instanceof AddrSpace )
+      && this.#name === other.name
+      && this.#value === other.value
+  }
 }
+
+module.exports = AddrSpace
