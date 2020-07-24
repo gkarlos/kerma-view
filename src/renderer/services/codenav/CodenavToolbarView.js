af5fa4daf615755b8e86a5ddc6ef2a1f7649207a
gkarlos <gkarloscodes@gmail.com>
2020-07-24 23:49:30 +0200

    [codenav] cleanup

--- src/renderer/services/codenav/CodenavToolbarView.js
+++ src/renderer/services/codenav/CodenavToolbarView.js
@@ -87,44 +87,9 @@ class CodenavToolbarView extends Component {
     
     this.node = $(`<div id='${this.id}'></div>`).appendTo(this.container)
 
-    // let buttonGroup = $(`<div class="btn-group btn-group-sm" role="group" id="codenav-toolbar-button-group"></div>`).appendTo(this.node)
-
     this._createStartStopButtons(this.node)
     this._createRestartButton(this.node)
-
-    // this.separator1 = $(`
-    //   <div id="codenav-toolbar-separator" role="group" class="border-left d-sm-none d-md-block"></div>
-    // `).appendTo(this.node)
-
     this._createNavButtons(this.node)
-
-    
-    
-    
-
-    // this.navButtons = $(`
-    //   <div class="btn-group btn-group" role="group" aria-label="Second group">
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-redo"></i></button>
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-angle-double-down"></i></button>
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-angle-double-down"></i></button>
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-angle-double-right"></i></button>
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-chevron-circle-down"></i></button>
-    //     <button type="button" class="btn btn btn-secondary"><i class="fas fa-chevron-circle-right"></i></button>
-    //   </div>`).appendTo(this.node)
-    //         .css('float', 'right')
-    //         .css('margin-top', '1px')
-    //         // .css('margin-right', '1px')
-    //         // .css('margin', '5px')
-
-    // this.stopRefreshButtons = $(`
-    //   <div class="btn-group btn-group" role="group" aria-label="First group" id="start-stop-container">
-    //     <button type="button" class="btn btn btn-info ml-auto" id="start-button"><i class="fas fa-play"></i> Start</button>
-    //     <button type="button" class="btn btn-secondary" id="stop-button"><i class="fas fa-retweet"></i></i></button>
-    //     <button type="button" class="btn btn-secondary" id="stop-button"><i class="fas fa-stop"></i></button>
-    //   </div>`).appendTo(this.node)
-    //           .css('float', 'right')
-    //           .css('margin-right', '2px')
-    //           .css('margin-top', '1px')
 
     this.rendered = true
 
