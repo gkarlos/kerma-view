3e17497396c798afb1adc7989cb30c78d815b4bb
gkarlos <gkarloscodes@gmail.com>
2020-08-10 23:58:50 +0200

    [memory-vis] added grid navigation buttons

--- src/renderer/services/memory-vis/view/MemoryVisViewGridToolbar.js
+++ src/renderer/services/memory-vis/view/MemoryVisViewGridToolbar.js
@@ -11,6 +11,11 @@ class MemoryVisViewGridToolbar {
   /** @type {JQuery}        */ #incSizeButton
   /** @type {JQuery}        */ #decSizeButton
   /** @type {JQuery}        */ #resetSizeButton
+  /** @type {JQuery}        */ #leftButton
+  /** @type {JQuery}        */ #rightButton
+  /** @type {JQuery}        */ #upButton
+  /** @type {JQuery}        */ #downButton
+  
   
   /**
    * @param {MemoryVisView} view 
@@ -23,6 +28,11 @@ class MemoryVisViewGridToolbar {
   isRendered() { return this.#rendered }
 
   _createZoomButtons() {
+    this.#leftButton  = $(`<button type="button" class="btn kv-btn kv-btn-xs memory-vis-toolbar-button-left"><i class="fas fa-angle-left"></i></i></button>`).appendTo(this.#node)
+    this.#upButton    = $(`<button type="button" class="btn kv-btn kv-btn-xs memory-vis-toolbar-button-up"><i class="fas fa-angle-up"></i></button>`).appendTo(this.#node)
+    this.#downButton  = $(`<button type="button" class="btn kv-btn kv-btn-xs memory-vis-toolbar-button-down"><i class="fas fa-angle-down"></i></i></button>`).appendTo(this.#node)
+    this.#rightButton = $(`<button type="button" class="btn kv-btn kv-btn-xs memory-vis-toolbar-button-right"><i class="fas fa-angle-right"></i></i></button>`).appendTo(this.#node)
+
     this.#incSizeButton = $(`<button type="button" class="btn kv-btn kv-btn-xs"><i class="fas fa-search-plus"></i></button>`).appendTo(this.#node)
     this.#decSizeButton = $(`<button type="button" class="btn kv-btn kv-btn-xs"><i class="fas fa-search-minus"></i></button>`).appendTo(this.#node)
     this.#resetSizeButton = $(`<button type="button" class="btn kv-btn kv-btn-xs"><i class="fas fa-compress" title="Reset"></i></button>`).appendTo(this.#node)
