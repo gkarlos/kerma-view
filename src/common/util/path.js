1129ca82fe24009fe21084ee82731ee46a215ee5
George Karlos <gkarloscodes@gmail.com>
2020-10-31 23:56:13 +0100

    [common][util] fix path

--- src/common/util/path.js
+++ src/common/util/path.js
@@ -3,7 +3,7 @@ const path = require('path')
 function resolve(filepath) {
   if (filepath[0] === '~')
       return path.join(process.env.HOME, path.resolve(filepath.slice(1)));
-  return path.resolve(filepath);
+  return filepath
 }
 
 module.exports = {
